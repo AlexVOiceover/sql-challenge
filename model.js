846f20f0949894dd84ce864d3d1843795b38801d
AlexVOiceover <alex@alexvoiceover.com>
2024-06-10 11:01:02 +0100

    Finito

--- model.js
+++ model.js
@@ -1,49 +1,58 @@
-const db = require("./database/db.js");
+const db = require('./database/db.js')
 
 const select_cohorts_in_finsbo = db.prepare(/*sql*/ `
-  -- [1]
-`);
+  SELECT name FROM cohorts WHERE location = 'Finsbury Park'
+`)
 
 function listCohortsInFinsbo() {
-  return select_cohorts_in_finsbo.all();
+	return select_cohorts_in_finsbo.all()
 }
 
 const select_students_in_finsbo = db.prepare(/*sql*/ `
-  -- [2]
-`);
+  SELECT username FROM students JOIN cohorts 
+  ON students.cohort_name = cohorts.name  WHERE location = 'Finsbury Park'
+`)
 
 function listStudentsInFinsbo() {
-  return select_students_in_finsbo.all();
+	return select_students_in_finsbo.all()
 }
 
 const select_students_with_location = db.prepare(/*sql*/ `
-  -- [3]
-`);
+  SELECT students.username, cohorts.location FROM students JOIN cohorts
+   WHERE students.cohort_name = cohorts.name
+`)
 
 function listStudentsWithLocation() {
-  return select_students_with_location.all();
+	return select_students_with_location.all()
 }
 
 const select_students_with_projects = db.prepare(/*sql*/ `
-  -- [4]
-`);
+  SELECT projects.name, students.username FROM students JOIN students_projects 
+  ON students.username = students_projects.student_username JOIN projects 
+  ON projects.id = students_projects.project_id
+`)
 
 function listStudentsWithProjects() {
-  return select_students_with_projects.all();
+	return select_students_with_projects.all()
 }
 
 const select_students_with_projects_in_finsbo = db.prepare(/*sql*/ `
-  -- [5]
-`);
+  SELECT projects.name, students.username
+  FROM students JOIN students_projects ON students.username = students_projects.student_username
+  JOIN projects ON students_projects.project_id = projects.id
+  JOIN cohorts ON cohorts.name = students.cohort_name
+  WHERE cohorts.location = 'Finsbury Park'
+  ORDER by username
+`)
 
 function listStudentsWithProjectsInFinsbo() {
-  return select_students_with_projects_in_finsbo.all();
+	return select_students_with_projects_in_finsbo.all()
 }
 
 module.exports = {
-  listCohortsInFinsbo,
-  listStudentsInFinsbo,
-  listStudentsWithLocation,
-  listStudentsWithProjects,
-  listStudentsWithProjectsInFinsbo,
-};
+	listCohortsInFinsbo,
+	listStudentsInFinsbo,
+	listStudentsWithLocation,
+	listStudentsWithProjects,
+	listStudentsWithProjectsInFinsbo,
+}
